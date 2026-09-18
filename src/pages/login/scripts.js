0693cd4c5d3d3fec2dffb935b8de2f3a244f0e04
Rickctrl1 <lopesricardo276@gmail.com>
2026-09-18 17:53:23 -0300

    Reorganizado
    reorganizado arquivos e pastas usando o método features structures.

---
+++ src/pages/login/scripts.js
@@ -0,0 +1,17 @@
+//script de visualização de senha
+
+//selecionando os elementos do HTML
+const inputSenha = document.getElementById('senha');
+const btnSenha = document.getElementById('btn-senha');
+
+//Adicionando evento de clique no botão
+btnSenha.addEventListener('click', function(){
+    //Verifica qual é o tipo atual do input
+    if (inputSenha.type === 'password') {
+        //Se for senha, muda para texto (fica visível)
+        inputSenha.type = 'text';
+    } else {
+        //Se for texto, muda de volta para senha (fica oculta)
+        inputSenha.type = 'password'
+    }
+});
